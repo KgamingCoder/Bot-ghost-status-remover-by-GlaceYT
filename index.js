3f6212a21c8c46dc9531c7635d69c54aa9a365a0
KgamingCoder <blueflower.kristian2011+github@gmail.com>
2024-10-19 08:51:33 +0200

    index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.listen(port, () => {
 });
 
 const statusMessages = ["passangers and DM's"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusTypes = [ 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
